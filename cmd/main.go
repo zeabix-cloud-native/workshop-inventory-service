796eae50e8593bcecdbbab7798289086d1a89778
Prawit Chaivong <prawit.c@zeabix.com>
2023-09-19 03:59:26 +0700

    Add env and manifest

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,22 @@ import (
 	"github.com/zeabix-cloud-native/workshop-inventory-service/internal/adapters/repository"
 	"github.com/zeabix-cloud-native/workshop-inventory-service/internal/core/services"
 	"github.com/zeabix-cloud-native/workshop-inventory-service/utils"
+
+	"fmt"
+	"log"
 )
 
 func main() {
+	port := utils.GetEnv("PORT", "3000")
+	initFile := utils.GetEnv("INIT_DB_FILE", "sample.json")
+
 	repo := repository.NewMapDBRepository()
-	utils.InitDB("sample.json", repo)
+	log.Printf("InitDB from %s\n", initFile)
+	utils.InitDB(initFile, repo)
 
 	srv := services.NewInventoryService(repo)
 	handler := httpv1.NewHTTPHandler(srv)
-	handler.Serve(":3030")
+
+	log.Printf("Start inventory service at port: %s\n", port)
+	handler.Serve(fmt.Sprintf(":%s", port))
 }
